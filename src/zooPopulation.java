0a9437863d57c46c30c409074307a446a7fbc9af
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-03-11 00:13:19 -0800

    cppzoo

---
+++ src/zooPopulation.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class zooPopulation {
+    public static void main(String[] args) throws IOException {
+        String path = "animalNames.txt";
+        String line = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            while ((line = br.readLine()) != null)
+            {
+            String[] values = line.split (",");
+            System.out.println(values[11]);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
