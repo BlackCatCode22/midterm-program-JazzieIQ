3a490eec5e7dcd127a33282851d2c0056e5c216d
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-03-11 18:03:34 -0800

    cppZoo

---
+++ src/zooPopulationIoTest.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class zooPopulation {
+    public static void main(String[] args) throws IOException {
+        String path = "animalNames.txt";
+        String line;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            while ((line = br.readLine()) != null) {
+                String[][] values;
+                values = new String[][]{line.split(":" + ",")};
+                System.out.println(values[0][0]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
+    /*static String genUniqueID (String speciesName, int numOfSpecies){
+        String uniqueID;
+        Switch (speciesName); {
+            case "hyena"
+                uniqueID = "Hy0" + numOfSpecies;
+            break;
+            case "lion"
+                uniqueID = "Li0" + numOfSpecies;
+            break;
+            case "tiger"
+                uniqueID = "Ti0" + numOfSpecies;
+            break;
+            case "bear"
+                uniqueID = "Be0" + numOfSpecies;
+            break;
+        }
+        return uniqueID;
+    }
+}
+*/
