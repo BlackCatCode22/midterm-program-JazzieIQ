31e0bb3fb0056358652c8cb2edbbb59490c4d850
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-03-14 21:35:02 -0700

    cppZoo

--- src/zooPopulation.java
+++ src/zooPopulation.java
@@ -1,118 +1,134 @@
-import com.sun.source.tree.ReturnTree;
-
 import java.io.*;
 import java.util.Scanner;
 
+import static java.lang.Integer.*;
+import static java.lang.System.*;
+
 public class zooPopulation {
+
+    static String calcBirth(int yearsOld, String birthSeason) {
+        int year = 2023 - yearsOld;
+        String monthDay = switch (birthSeason) {
+            case "spring," -> "03-19";
+            case "summer," -> "5-21";
+            case "fall," -> "08-19";
+            case "winter," -> "12-19";
+            default -> "01-01";
+        };
+        String newDate = year + "-" + monthDay;
+        return newDate;
+    }
+
+    static String genUniqueID(String speciesName, int numOfSpecies) {
+        return switch (speciesName) {
+            case "hyena" -> "Hy0" + numOfSpecies;
+            case "lion" -> "Li0" + numOfSpecies;
+            case "tiger" -> "Ti0" + numOfSpecies;
+            case "bear" -> "Be0" + numOfSpecies;
+            default -> "error";
+        };
+    }
+
+
     public static void main(String[] args) throws IOException {
-        System.out.println("\n\nWelcome to Mac Mac's Zoo program!\n\n" + "I will be using the following info:\n\n");
+        out.println("""
+
+                Welcome to Mac Mac's Zoo program!
+
+                I will be using the following info:
+
+                """);
         String animalsNames = "animalNames.txt";
         String names;
         String arrivingAnimals = "arrivingAnimals.txt";
-        String animals;
+        String arriving;
         try {
             BufferedReader br = new BufferedReader(new FileReader(animalsNames));
-
             while ((names = br.readLine()) != null) {
                 String[][] values;
                 values = new String[][]{names.split(":" + ",")};
-                System.out.println(values[0][0]);
+                out.println(values[0][0]);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-        System.out.println("\n");
+        out.println("\n");
         try {
             BufferedReader br = new BufferedReader(new FileReader(arrivingAnimals));
-
-            while ((animals = br.readLine()) != null) {
+            while ((arriving = br.readLine()) != null) {
                 String[][] values;
-                values = new String[][]{animals.split(":" + ",")};
-                System.out.println(values[0][0]);
-
+                values = new String[][]{arriving.split(":" + ",")};
+                out.println(values[0][0]);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-        System.out.println("\nLet's begin!\n");
-
-        static String calcBirth ( int yearsOld, String birthSeason)
-        {
-            int year = 2023 - yearsOld;
-            String monthDay;
-            String newDate;
-
-            switch (birthSeason) {
-                case "spring,":
-                    monthDay = "03-19";
-                    break;
-                case "summer,":
-                    monthDay = "04-19";
-                    break;
-                case "fall,":
-                    monthDay = "08-19";
-                    break;
-                case "winter,":
-                    monthDay = "12-19";
-                    break;
-                default:
-                    monthDay = "01-01";
-                    break;
+        String[] animals = new String[16];
+        int lineNum = 0;
+        try {
+            File myAnimals = new File("arrivingAnimals.txt");
+            Scanner scanner = new Scanner(myAnimals);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                animals[lineNum] = line;
+                lineNum++;
+                out.println(line);
             }
-            newDate = Integer.toString(year) + "-" + monthDay;
-
-            return newDate;
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            out.println("\n A file error occured...");
+            e.printStackTrace();
         }
-
-        static String genUniqueID (String speciesName,int numOfSpecies){
-            String uniqueID;
-
-            switch (speciesName) {
-                case "hyena":
-                    uniqueID = "Hy0" + Integer.toString(numOfSpecies);
-                    break;
-                case "lion":
-                    uniqueID = "Li0" + Integer.toString(numOfSpecies);
-                    break;
-                case "tiger":
-                    uniqueID = "Ti0" + Integer.toString(numOfSpecies);
-                    break;
-                case "bear":
-                    uniqueID = "Be0" + Integer.toString(numOfSpecies);
-                    break;
-                default:
-                    uniqueID = "error";
-                    break;
-            }
-            return uniqueID;
-        }
+        out.println("\nLet's begin!\n");
         int numOfHyenas = 0;
         int numOfLions = 0;
         int numOfTigers = 0;
         int numOfBears = 0;
-        int lineNum = 0;
-
-        String[] zooAnimals = new String[16] () {
-            try {
-                File myAnimalsFile = new File("arrivingAnimals.txt");
-                Scanner scanner = new Scanner(myAnimalsFile);
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    zooAnimals[lineNum] = line;
-                    lineNum++;
-                    System.out.println(line);
-                }
-                scanner.close();
-            } catch (FileNotFoundException e ;){
-                System.out.println("\n A file error occured...");
-                e.printStackTrace();
-            }
-            //System.out.println("\n The array...\n");
-            for (String line : zooAnimals) {
-                System.out.println(line);
-            }
+        int years = 0;
+        String season;
+        String sex;
+        String species;
+        String color;
+        String origin;
+        String weight;
+        String name;
+        String uniqueID = "xyz";
+        String arrived;
+        String[] splitAnimals = animals[4].split(" ", 0);
+        for (String s: splitAnimals) {
+            //System.out.println(s);
         }
+        int birthdate = parseInt(calcBirth(Integer.parseInt((splitAnimals[0])), splitAnimals[7]));
+        System.out.println("birthDate is: " + birthdate);
+        sex = splitAnimals[3];
+        System.out.println("sex is: " + sex);
+        species = splitAnimals[4];
+        System.out.println("species is: " + species);
+        int position = species.indexOf(",");
+        System.out.println("position of comma is: " + position);
+        species = species.substring(0, position);
+        System.out.println("species is now: " + species);
+        if (species.equals("hyena")) {
+            numOfHyenas++;
+            uniqueID = genUniqueID(species, numOfHyenas);
+        } else if (species.equals("lion")) {
+            numOfLions++;
+            uniqueID = genUniqueID(species, numOfLions);
+        } else if (species.equals("tiger")) {
+            numOfTigers++;
+            uniqueID = genUniqueID(species, numOfTigers);
+        } else if (species.equals("bear")) {
+            numOfBears++;
+            uniqueID = genUniqueID(species, numOfBears);
+        } else {
+            out.println("Error tabulating number of species");
+        }
+        System.out.println("numOfHyenas = " + numOfHyenas);
+        System.out.println("numOfLions = " + numOfLions);
+        System.out.println("numOfTigers = " + numOfTigers);
+        System.out.println("numOfBears = " + numOfBears);
+        System.out.println("uniqueID = " + uniqueID);
     }
 }
