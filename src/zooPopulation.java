a0c1877e02beba9e98154b585e1e6ddc3e16cb24
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-03-16 01:12:33 -0700

    cppZoo

--- src/zooPopulation.java
+++ src/zooPopulation.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
-import static java.lang.Integer.*;
+
 import static java.lang.System.*;
 public class zooPopulation {
     static String calcBirth(int yearOld, String birthSeason) {
@@ -51,19 +51,50 @@ public class zooPopulation {
             throw new RuntimeException(e);
         }
         out.println("\n");
-        int lineNum = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(arrivingAnimals));
+            while ((arriving = br.readLine()) != null) {
+                String[][] values;
+                values = new String[][]{arriving.split(":" + ",")};
+                out.println(values[0][0]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        out.println("\n");
+
+       int idLine = 0;
+       String [] id = new String [7];
+       try {
+            File animalId = new File (animalsNames);
+            Scanner scanner = new Scanner(animalId);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                id[idLine] = line;
+                idLine ++;
+                //out.println(line);
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException e) {
+            out.println("\n A file error occured...");
+            e.printStackTrace();
+        }
+       int lineNum = 0;
         String[] animals = new String[16];
         try {
-            File myAnimals = new File("arrivingAnimals.txt");
+            File myAnimals = new File(arrivingAnimals);
             Scanner scanner = new Scanner(myAnimals);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 animals[lineNum] = line;
                 lineNum++;
-                out.println(line);
+                //out.println(line);
             }
             scanner.close();
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             out.println("\n A file error occured...");
             e.printStackTrace();
         }
@@ -89,43 +120,51 @@ public class zooPopulation {
         /*for (String s: splitAnimals) {
             System.out.println(s);
         }*/
+            String yearsOld = splitAnimals[0];
+            species = splitAnimals[4];
+            int position = species.indexOf(",");
+            species = species.substring(0, position);
+            System.out.println("species is: " + species);
+            //System.out.println("position of comma is: " + position);
             String birthdate = calcBirth(Integer.parseInt(splitAnimals[0]), splitAnimals[7]);;
             System.out.println("birthDate is: " + birthdate);
+            System.out.println("age is: " + yearsOld);
             sex = splitAnimals[3];
             System.out.println("sex is: " + sex);
-            species = splitAnimals[4];
-            System.out.println("species is: " + species);
-            int position = species.indexOf(",");
-            System.out.println("position of comma is: " + position);
-            species = species.substring(0, position);
-            System.out.println("species is now: " + species);
             color = splitStrComma[2];
-            System.out.println("color = " + color);
+            System.out.println("color ; " + color);
             weight = splitStrComma[3];
             origin = splitStrComma[4] + "," + splitStrComma[5];
-            System.out.println("weight = " + weight);
-            System.out.println("origin = " + origin);
+            System.out.println("weight ; " + weight);
+            System.out.println("origin ; " + origin);
             if (species.equals("hyena")) {
                 numOfHyenas++;
                 uniqueID = genUniqueID(species, numOfHyenas);
+                System.out.println("uniqueID = " + uniqueID);
             } else if (species.equals("lion")) {
                 numOfLions++;
                 uniqueID = genUniqueID(species, numOfLions);
+                System.out.println("uniqueID = " + uniqueID);
             } else if (species.equals("tiger")) {
                 numOfTigers++;
                 uniqueID = genUniqueID(species, numOfTigers);
+                System.out.println("uniqueID = " + uniqueID);
             } else if (species.equals("bear")) {
                 numOfBears++;
                 uniqueID = genUniqueID(species, numOfBears);
+                System.out.println("uniqueID = " + uniqueID);
             } else {
                 out.println("Error tabulating number of species");
             }
             out.println("\n");
+            //return;
         }
+        //for (int i = 0; i<4; i++) {
+        // string [] splitNames =
+        // }
         System.out.println("numOfHyenas = " + numOfHyenas);
         System.out.println("numOfLions = " + numOfLions);
         System.out.println("numOfTigers = " + numOfTigers);
         System.out.println("numOfBears = " + numOfBears);
-        System.out.println("uniqueID = " + uniqueID);
     }
 }
